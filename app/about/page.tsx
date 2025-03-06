8b4335c3701b38b1dfd720697a40aea359bac9e7
DonCuervoC <nelson.cuervo89@gmail.com>
2025-03-06 11:37:01 -0500

    About page responsive OK

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,19 +7,18 @@
 // import { button as buttonStyles } from "@heroui/theme";
 
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
+import { title } from "@/components/primitives";
 
 import Image from "next/image";
 
 export default function AboutPage() {
   return (
     <section className="flex flex-col items-center justify-center gap-4">
+      {/* Sección principal con flexbox, centrado de elementos y espacio entre ellos */}
       <div className="absolute inset-0 z-0">
-        <div className="absolute inset-0 
-              bg-gradient-radial
-               from-black via-transparent
-                to-black opacity-10">
-
+        {/* Contenedor absoluto que ocupa todo el espacio disponible */}
+        <div className="absolute inset-0 bg-gradient-radial from-black via-transparent to-black opacity-10">
+          {/* Fondo con gradiente radial y opacidad ajustada */}
           <Image
             alt="Background02"
             src="/background/backg-04.jpg"
@@ -30,13 +29,16 @@ export default function AboutPage() {
           />
         </div>
       </div>
-      <div className="irelative z-10 inline-block max-w-4xl text-center">
+      <div className="relative z-10 inline-block max-w-4xl text-center">
+        {/* Contenedor principal con posición relativa, z-index, y centrado de texto */}
         <span className={title()}>TATTOO&nbsp;</span>
         <span className={title({ color: "yellow" })}>ARTIST&nbsp;</span>
         <br />
         <br />
-        <div className="flex flex-row justify-center items-start gap-4">
+        <div className="flex flex-col md:flex-row justify-center items-start gap-4">
+          {/* Contenedor flex con dirección de columna en pantallas pequeñas y fila en medianas y grandes */}
           <div className="flex-shrink-0" style={{ width: '400px' }}>
+            {/* Contenedor de imagen con tamaño fijo */}
             <Image
               alt="Nicolas-Cuervo"
               height={400} // Tamaño de la imagen
@@ -45,6 +47,7 @@ export default function AboutPage() {
             />
           </div>
           <div className="flex-shrink-0" style={{ width: '420px' }}>
+            {/* Contenedor de texto con tamaño fijo */}
             <p className={`text-justify`} style={{ fontFamily: 'var(--font-roboto)' }}>
               {siteConfig.description}
             </p>
